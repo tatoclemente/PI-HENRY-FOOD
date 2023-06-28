a690b1288012a6265e257132f8a4c1486bc3920a
tatoclemente <tatoapuros@gmail.com>
2023-06-28 13:58:16 -0300

    Cookie Parser SameSite=None

--- api/src/app.js
+++ api/src/app.js
@@ -14,8 +14,10 @@ server.name = 'API';
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
+
 server.use(morgan('dev'));
 server.use((req, res, next) => {
+  res.setHeader('Set-Cookie', 'cross-site-cookie=whatever; SameSite=None; Secure');
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
