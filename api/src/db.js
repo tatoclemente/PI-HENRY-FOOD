823bb9f4246a68c300195a8b659cbf5f7c686ca2
tatoclemente <tatoapuros@gmail.com>
2023-09-11 15:00:43 -0300

    Deplor new Version

--- api/src/db.js
+++ api/src/db.js
@@ -15,6 +15,10 @@ const sequelize = new Sequelize(DB_DEPLOY, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
